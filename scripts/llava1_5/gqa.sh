5791996e064788aba68581229c9c372b2ebde26d
MaQianKun0 <maqiankun2018@gmail.com>
2026-02-23 23:28:07 +0800

    initial commit

--- scripts/llava1_5/gqa.sh
+++ scripts/llava1_5/gqa.sh
@@ -7,14 +7,14 @@ CHUNKS=${#GPULIST[@]}
 
 CKPT="llava-v1.5-7b"
 SPLIT="llava_gqa_testdev_balanced"
-GQADIR="/home/mqk/VScan/data/eval/gqa"
+GQADIR="data/eval/gqa"
 
 for IDX in $(seq 0 $((CHUNKS-1))); do
     CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m llava.eval.model_vqa_loader \
-        --model-path /home/mqk/VScan/data/model/llava-v1.5-7b \
-        --question-file /home/mqk/VScan/data/eval/gqa/$SPLIT.jsonl \
-        --image-folder /home/mqk/VScan/data/eval/gqa/images \
-        --answers-file /home/mqk/VScan/data/eval/gqa/answers/$SPLIT/$CKPT/${CHUNKS}_${IDX}.jsonl \
+        --model-path data/model/llava-v1.5-7b \
+        --question-file data/eval/gqa/$SPLIT.jsonl \
+        --image-folder data/eval/gqa/images \
+        --answers-file data/eval/gqa/answers/$SPLIT/$CKPT/${CHUNKS}_${IDX}.jsonl \
         --num-chunks $CHUNKS \
         --chunk-idx $IDX \
         --temperature 0 \
@@ -27,14 +27,14 @@ done
 
 wait
 
-output_file=/home/mqk/VScan/data/eval/gqa/answers/$SPLIT/$CKPT/merge.jsonl
+output_file=data/eval/gqa/answers/$SPLIT/$CKPT/merge.jsonl
 
 # Clear out the output file if it exists.
 > "$output_file"
 
 # Loop through the indices and concatenate each file.
 for IDX in $(seq 0 $((CHUNKS-1))); do
-    cat /home/mqk/VScan/data/eval/gqa/answers/$SPLIT/$CKPT/${CHUNKS}_${IDX}.jsonl >> "$output_file"
+    cat data/eval/gqa/answers/$SPLIT/$CKPT/${CHUNKS}_${IDX}.jsonl >> "$output_file"
 done
 
 python scripts/convert_gqa_for_eval.py --src $output_file --dst $GQADIR/testdev_balanced_predictions.json
